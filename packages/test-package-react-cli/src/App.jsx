a62172e876e715c41001df51defb639016c00f68
JackySummer <1106619871@qq.com>
2022-04-19 23:21:03 +0800

    feat: add test-package-jacky-utils deps to react-cli lib

--- packages/test-package-react-cli/src/App.jsx
+++ packages/test-package-react-cli/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import { add } from 'test-package-jacky-utils'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return <div className="App">test package</div>
+  return <div className="App">test package {console.log(add(1, 2))}</div>
 }
 
 export default App
